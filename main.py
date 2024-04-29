bca90e274b259f84960e0aab658b34fdc12cff27
Lukasz Kozarski <luke.kozarski@gmail.com>
2024-04-29 18:44:04 +0200

    przeniesienie stalych i funkcji plikowych do osobnego modulu

--- main.py
+++ main.py
@@ -11,50 +11,12 @@ Program biblioteczny:
 # 6. LICZBA    Wypisanie sumy liczb woluminow w bibliotece
 # 7. KONIEC    Zakonczenie dzialania programu
 """
-
-DEFAULT_HISTORY_FILE_PATH = 'data/historia.txt'
-DEFAULT_LIBRARY_FILE_PATH = 'data/library.txt'
-
-def wczytaj_historie(file_path=DEFAULT_HISTORY_FILE_PATH):
-    """Format historii:
-    Wpis 1
-    Wpis 2
-    Wpis 3
-    ..."""
-    historia = []
-    with open(file_path) as f:
-        for line in f:
-            historia.append(line.strip())
-    return historia
-
-
-def zapisz_historie(historia, file_path=DEFAULT_HISTORY_FILE_PATH):
-    """Format historii:
-    Wpis 1
-    Wpis 2
-    Wpis 3
-    ..."""
-    with open(file_path, "w") as f:
-        for line in historia:
-            f.write(str(line) + '\n')
-
-
-def wczytaj_biblioteke(file_path=DEFAULT_LIBRARY_FILE_PATH):
-    """Format biblioteki:
-    Hobbit;10
-    Harry Potter i Komnata Tajemnic;25"""
-    biblioteka = {}
-    with open(file_path) as f:
-        for line in f:
-            tytul, sztuk = line.strip().split(';')
-            biblioteka[tytul] = int(sztuk)
-    return biblioteka
-
-
-def zapisz_biblioteke(biblioteka, file_path=DEFAULT_LIBRARY_FILE_PATH):
-    with open(file_path, "w") as f:
-        for tytul, sztuk in biblioteka.items():
-            f.write(f"{tytul};{sztuk}\n")
+from modules.io import (
+    wczytaj_historie,
+    zapisz_historie,
+    wczytaj_biblioteke,
+    zapisz_biblioteke,
+)
 
 
 slownik_ksiazek = wczytaj_biblioteke()
---
+++ modules/settings.py
@@ -0,0 +1,2 @@
+DEFAULT_HISTORY_FILE_PATH = 'data/historia.txt'
+DEFAULT_LIBRARY_FILE_PATH = 'data/library.txt'
